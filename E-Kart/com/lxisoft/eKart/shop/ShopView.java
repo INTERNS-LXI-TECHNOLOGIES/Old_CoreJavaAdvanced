bdfdf0149d3d79da6ed0027293708311c7f8bba6
balagopal890 <balagopal.v@lxisoft.com>
2018-09-20 16:36:11 +0530

    first iteration

---
+++ E-Kart/com/lxisoft/eKart/shop/ShopView.java
@@ -0,0 +1,15 @@
+package com.lxisoft.eKart.shop;
+
+import java.io.*;
+
+public class ShopView{
+	
+	public ShopView(){
+		
+		System.out.println("\t\t\t\t\t WELCOME TO eKART \t\t\t\n\n");
+		System.out.println("\t\t\t\tPLEASE SELECT THE PRODUCT FROM THE PRODUCT LIST\t\t\t\n\n");
+	    System.out.println("\t\t [1.BASKET ] \t\t [2.RICE] \t\t [3.BROOM] \t\t [4.BISCUIT]\n\n");
+		
+	}
+	
+}
---
+++ E-Kart/com/lxisoft/eKart/test/Test.java
@@ -0,0 +1,11 @@
+package com.lxisoft.eKart.test;
+
+import java.io.*;
+
+import com.lxisoft.eKart.shop.*;
+
+public class Test{
+	public static void main(String args[]){
+	ShopController shop = new ShopController();
+	}
+}
